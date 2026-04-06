db4a432f7ff0802815c0bb41d553e829b5d970dd
sinan <sinan@bpract.com>
2026-04-06 10:53:18 +0530

    feat: logo changes and unwanted file removed

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,5 +2,34 @@ export const CATEGORIES = [
   { label: "Men", value: "Men" },
   { label: "Women", value: "Women" },
   { label: "Kids", value: "Kids" },
-  {label:"test",value:"test"}
+];
+
+export const MEN_CATEGORIES = [
+  { label: "Men's Shirts", value: "mens_shirts" },
+  { label: "Men's Pants", value: "mens_pants" },
+  { label: "Men's T-Shirts", value: "mens_tshirts" },
+  { label: "Mundu", value: "mundu" },
+  { label: "Innerwear", value: "mens_innerwear" },
+];
+
+export const WOMEN_CATEGORIES = [
+  { label: "Sarees", value: "sarees" },
+  { label: "Nighties", value: "nighties" },
+  { label: "Churidar", value: "churidar" },
+  { label: "Kurtis", value: "kurtis" },
+  { label: "Leggings", value: "leggings" },
+  { label: "Innerwear", value: "womens_innerwear" },
+];
+
+export const KIDS_CATEGORIES = [
+  { label: "Girls Wear", value: "girls_wear" },
+  { label: "Boys Wear", value: "boys_wear" },
+  { label: "Baby Wear", value: "baby_wear" },
+  { label: "School Uniforms", value: "school_uniforms" },
+];
+
+export const ALL_CATEGORIES = [
+  ...MEN_CATEGORIES,
+  ...WOMEN_CATEGORIES,
+  ...KIDS_CATEGORIES,
 ];
